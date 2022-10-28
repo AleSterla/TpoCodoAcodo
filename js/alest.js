83225fe3c8716e7cb70857628ef1bb639d444a9b
AleSterla <alejandro-st@hotmail.com>
2022-10-27 23:16:15 -0300

    Sin tiempo de mas, pero habia mucho más por hacer!

--- js/alest.js
+++ js/alest.js
@@ -37,3 +37,152 @@ function trainspotting(valor){
     contactform.values3.value=num3;
     //alert(num1+' '+num2+' '+num3);
 }
+let banderin=1;
+function Classalirse(){
+    return;
+    /*pendiente: quitar los a y agregar button para el efecto al salir*/
+    //   setTimeout(() => {
+    //     console.log("1 Segundo esperado")
+    //   }, 10000);
+
+      setInterval(() => {
+        console.log("Ejecutar cada 10 seg");
+        cambiaclase();
+      }, 5000);      
+
+    //   window.clearInterval()
+}
+var sPath = window.location.pathname;
+var sPage = sPath.substring(sPath.lastIndexOf('/') + 1);
+if(sPage == "index.html"){
+    setInterval(() => {
+        cambiaclase();
+    }, 3000);      
+}
+
+function cambiaclase(){
+    if(banderin==1){
+        mueve.className="animate__animated animate__backInUp";
+        console.log("animate__backInUp");
+        banderin++;
+    } else if(banderin==2){
+        mueve.className="animate__animated animate__backInDown";
+        console.log("animate__backInDown");
+        banderin++;
+    } else if(banderin==3){
+        mueve.className="animate__animated animate__backInRight";
+        console.log("animate__backInRight");
+        banderin++;
+    } else if(banderin==4){
+        mueve.className="animate__animated animate__backInLeft";
+        console.log("animate__backInLeft");
+        banderin=1;
+    }
+}
+
+function verificaObjeto (objeto){
+	if(objeto.value==""){
+		alert("Debe completar el campo "+ objeto.getAttribute("nombre") + ", es obligatorio!!!!");
+		objeto.focus();
+		return true;
+	}
+}
+
+function tamano(campo){	
+	if (campo.value.length > 249)
+		return false;
+	else
+		return true;
+}
+
+function valida(){
+    if(verificaObjeto(contactform.name)) return false;
+    if(verificaObjeto(contactform.apellido)) return false;
+    if(verificaObjeto(contactform._replyto)) return false;
+    if(verificaObjeto(contactform.clave)) return false;
+    if(verificaObjeto(contactform.reclave)) return false;
+    if(contactform.clave.value!=contactform.reclave.value){
+        alert("No coinciden las Claves!");
+        contactform.clave.focus();
+        return false;
+    }
+    if(verificaObjeto(contactform.interesmayor)) return false;
+    if(verificaObjeto(contactform.message)) return false;
+}
+
+
+async function obtener(dia,cual){
+    try{
+        // const apiUrl = "https://api.nasa.gov/planetary/apod?date=2022-10-18&api_key=DEMO_KEY"
+        const apiUrl = "https://api.nasa.gov/planetary/apod?date="+dia+"&api_key=DEMO_KEY"
+        const response = await fetch(apiUrl);
+        const data = await response.json();
+        console.log(data);
+        console.log(data.code);
+        if(data.code== null){
+            if(cual=='1'){
+                document.getElementById("imagen").src = data.url;
+                document.getElementById("imagen").className='vis1';
+            }else{
+                document.getElementById("imagen2").src = data["hdurl"];
+                document.getElementById("imagen2").className='vis1';
+            }
+        }
+        else{
+            document.getElementById("errore").innerHTML ="La Nasa no nos deja ver tantas veces seguidas!";
+        }
+    }
+    catch (error){console.log('Ocurrió un error grave:', error);}
+}
+
+function mostrame(){
+    let valor=document.getElementById('fecha').value;
+    if(valor=='')
+        alert('Ingresá una fecha, daaalee!!!'); 
+    else {
+        obtener(valor,'2');
+        console.log(valor);
+    }
+
+}
+let hoy = fechahoy();
+
+function dalavuelta(){
+    var fecha='2022-12-06 02:55:00';
+    var fecha='2022-12-06';
+    console.log(fecha);
+    var nueva=fecha.split(" ")[0].split("-").reverse().join("-");
+    console.log(nueva);
+}
+
+function fechahoy(){
+    let date = new Date();
+    let day = date.getDate();
+    let month = date.getMonth() + 1;
+    let year = date.getFullYear();
+    if(month < 10){
+        if(day<10){
+            console.log(`${year}-0${month}-0${day}`);
+                return (`${year}-0${month}-0${day}`);
+        }else{
+            console.log(`${year}-0${month}-${day}`);
+                return (`${year}-0${month}-${day}`);
+        }
+    }else{
+        if(day<10){
+            console.log(`${year}-${month}-0${day}`);
+                return (`${year}-${month}-0${day}`);
+        }else{
+            console.log(`${year}-${month}-${day}`);
+                return (`${year}-${month}-${day}`);
+        }
+    }
+}
+
+if(sPage == "observatorios.html"){
+    // obtener(hoy,'1');
+    console.log(hoy);
+}
+
+
+// {"date":"2022-10-15","explanation":"Gamma-ray burst GRB 221009A likely signals the birth of a new black hole, formed at the core of a collapsing star long ago in the distant universe. The extremely powerful blast is depicted in this animated gif constructed using data from the Fermi Gamma Ray Space Telescope. Fermi captured the data at gamma-ray energies, detecting photons with over 100 million electron volts. In comparison visible light photons have energies of about 2 electron volts. A steady, high energy gamma-ray glow from the plane of our Milky Way galaxy runs diagonally through the 20 degree wide frame at the left, while the transient gamma-ray flash from GRB 221009A appears at center and then fades. One of the brightest gamma-ray bursts ever detected GRB 221009A is also close as far as gamma-ray bursts go, but still lies about 2 billion light-years away. In low Earth orbit Fermi\u2019s Large Area Telescope recorded gamma-ray photons from the burst for more than 10 hours as high-energy radiation from GRB 221009A swept over planet Earth last Sunday, October 9.","hdurl":"https://apod.nasa.gov/apod/image/2210/LAT_221009A_burst_opt_1080.gif","media_type":"image","service_version":"v1","title":"GRB 221009A","url":"https://apod.nasa.gov/apod/image/2210/LAT_221009A_burst_opt_1080.gif"}
